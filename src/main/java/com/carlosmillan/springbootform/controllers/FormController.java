fa174f279836511e1b9827d55f16640cd6e562f2
cronos88 <atlantis9@hotmail.com>
2023-12-02 22:21:09 -0500

    Validación del formulario usando anotaciones

--- src/main/java/com/carlosmillan/springbootform/controllers/FormController.java
+++ src/main/java/com/carlosmillan/springbootform/controllers/FormController.java
@@ -1,10 +1,15 @@
 package com.carlosmillan.springbootform.controllers;
 
 import com.carlosmillan.springbootform.models.domain.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -16,7 +21,7 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Usuario usuario, Model model){
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
 //                           @RequestParam String username,
 //                           @RequestParam String password,
 //                           @RequestParam String email) {
@@ -27,6 +32,17 @@ public class FormController {
 //        usuario.setEmail(email);
 
         model.addAttribute("titulo", "Resultado Form");
+
+        if(result.hasErrors()) {
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(), "El campo "
+                        .concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+            });
+            model.addAttribute("error", errores);
+            return "form"; //retorna el formulario si hay error en el mismo
+        }
+
         model.addAttribute("usuario", usuario);
 
         return "resultado";
